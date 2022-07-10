db452e9a6f9a43aef5cdd17c4f7648d3722cf21b
tokatu4561 <ryobsk4561@gmail.com>
2022-07-10 01:58:50 -0700

    start line bot

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tokatu4561/line-bot
+
+go 1.18
+
+require github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+func main() {
+	// LINE Botクライアント生成する
+	// BOT にはチャネルシークレットとチャネルトークンを環境変数から読み込み引数に渡す
+	bot, err := linebot.New(
+		os.Getenv("LINE_BOT_CHANNEL_SECRET"),
+		os.Getenv("LINE_BOT_CHANNEL_TOKEN"),
+	)
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// // weatherパッケージパッケージから天気情報の文字列をを取得する
+	// result, err := weather.GetWeather()
+	// エラーに値があればログに出力し終了する
+	if err != nil {
+		log.Fatal(err)
+	}
+	// テキストメッセージを生成する
+	message := linebot.NewTextMessage("Hello World")
+	// テキストメッセージを友達登録しているユーザー全員に配信する
+	if _, err := bot.BroadcastMessage(message).Do(); err != nil {
+		log.Fatal(err)
+	}
+}
